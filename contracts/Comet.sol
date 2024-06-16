b57824a9865e4d7b205ccbbe637cb328d8dc6457
ErvinDimitri <ervinluis84@gmail.com>
2024-06-16 03:24:41 +0200

    Feature: BaseSupplyIndex; Present Value; Principal Value

--- contracts/Comet.sol
+++ contracts/Comet.sol
@@ -20,4 +20,46 @@ contract Comet is CometMainInterface{
     function mulFactor( uint n, uint factor) internal pure returns( uint){
         return n*factor/FACTOR_SCALE;
     }
+
+    // @return the timestamp in uint40
+    function getNowInternal() virtual internal view returns(uint40){
+        if( block.timestamp >= 2**40) revert TimestampTooLarge();
+        return uint40(block.timestamp);
+    }
+    
+    // Calculate baseSupplyIndex and baseBorrowIndex
+    function accruedInterestIndices( uint timeElapsed) internal view returns( uint64, uint64){
+        uint64 baseSupplyIndex_ = baseSupplyIndex;
+        uint64 baseBorrowIndex_ = baseBorrowIndex;
+        if( timeElapsed > 0){
+            uint utilization = getUtilization();
+            uint supplyRate = getSupplyRate( utilization);
+            uint borrowRate = getBorrowRate( utilization);
+            baseSupplyIndex_ += safe64( mulFactor( baseSupplyIndex_, supplyRate * timeElapsed));
+            baseBorrowIndex_ += safe64( mulFactor( baseBorrowIndex_, borrowRate * timeElapsed));        
+        }
+        return ( baseSupplyIndex_, baseBorrowIndex_);
+    }
+
+    // Updates trackingSupplyIndex and trackingBorrowIndex
+    function accrueInternal() internal{
+        uint40 now_ = getNowInternal();
+        uint timeElapsed = uint256( now_ - lastAccrualTime);
+        if(timeElapsed > 0){
+            (baseSupplyIndex, baseBorrowIndex) = accruedInterestIndices( timeElapsed);
+            if( totalSupplyBase >= baseMinForRewards){
+                trackingSupplyIndex += safe64( divBaseWei(basTrackingSupplySpeed * timeElapsed, totalSupplyBase));                
+            }
+            if( totalBorrowBase >= baseMinForRewards){
+                trackingBorrowIndex += safe64(devBaseWei( baseTrackingBorrowSpeed * timeElapsed, totalBorrowBase));
+            }
+            lastAccrualTime = now_;
+        }
+    }
+
+    function balanceOf( address account) override public view returns( uint256){
+        (uint64 baseSupplyIndex_, ) = accruedInterestIndices( getNowInternal() - lastAccrualTime);
+        int104 principal = UserBasic[account].principal;
+        return principal > 0 ? presentValueSupply( baseSupplyIndex_, unsigned104( principal)) : 0;
+    }
 }
--- contracts/CometStorage.sol
+++ contracts/CometStorage.sol
@@ -3,9 +3,20 @@ pragma solidity ^0.8.9;
 
 // Stores vars used in Comet
 contract CometStorage {
-    uint64 internal baseSupplyIndex;
-    uint64 internal baseBorrowIndex;
+    struct UserBasic{
+        int64 principal;  // Value of dollar inside the Compound
+        uint64 baseTrackingIndex;  // similar to baseTrackingIndex; not in use
+        uint64 baseTrackingAccrued;  // used by CometRewards to determine the amount of COMP to reward
+        uint64 assetsIn;  // used by borrowers to know if a collateral is in use
+        uint8 _reserved;  // Not used
+    }
 
-    uint104 internal totalSupplyBase;
-    uint104 internal totalBorrowBase;
+    uint64 internal baseSupplyIndex;  // it's an "exchange rate" to swap present value to principal value and vice versa
+    uint64 internal baseBorrowIndex;
+    uint64 internal trackingSupplyIndex;
+    uint64 internal trackingBorrowIndex;
+    uint104 internal totalSupplyBase;  // sum of all amount deposited by lenders
+    uint104 internal totalBorrowBase;  // sum of all amount deposited by borrowers
+    uint40 internal lastAccrualTime;  // updated @ Comet::accrueInternal()
+    uint8 internal pauseFlags;
 }
