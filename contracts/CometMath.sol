3464f2a272d769dd592d53ef4ee4ca5ae990d6e7
ErvinDimitri <ervinluis84@gmail.com>
2024-06-17 02:20:17 +0200

    Feature: CometMath

--- contracts/CometMath.sol
+++ contracts/CometMath.sol
@@ -3,5 +3,55 @@ pragma solidity ^0.8.9;
 
 // Pure math functions
 contract CometMath{
+    error InvalidUInt64();
+    error InvalidUInt104();
+    error InvalidUInt128();
+    error InvalidInt104();
+    error InvalidInt256();
+    error NegativeNumber();
 
+    // -- SAFE: cast from uint256 into smaller uint
+    function safe64(uint n) internal pure returns (uint64) {
+        if (n > type(uint64).max) revert InvalidUInt64();
+        return uint64(n);
+    }
+
+    function safe104(uint n) internal pure returns (uint104) {
+        if (n > type(uint104).max) revert InvalidUInt104();
+        return uint104(n);
+    }
+
+    function safe128(uint n) internal pure returns (uint128) {
+        if (n > type(uint128).max) revert InvalidUInt128();
+        return uint128(n);
+    }
+
+    function signed104(uint104 n) internal pure returns (int104) {
+        if (n > uint104(type(int104).max)) revert InvalidInt104();
+        return int104(n);
+    }
+
+    function signed256(uint256 n) internal pure returns (int256) {
+        if (n > uint256(type(int256).max)) revert InvalidInt256();
+        return int256(n);
+    }
+
+    // ----- UNSIGNED: only converts values from int to uint if value>=0
+    function unsigned104(int104 n) internal pure returns (uint104) {
+        if (n < 0) revert NegativeNumber();
+        return uint104(n);
+    }
+
+    function unsigned256(int256 n) internal pure returns (uint256) {
+        if (n < 0) revert NegativeNumber();
+        return uint256(n);
+    }
+
+    function toUInt8(bool x) internal pure returns (uint8) {
+        return x ? 1 : 0;
+    }
+
+    function toBool(uint8 x) internal pure returns (bool) {
+        return x != 0;
+    }
 }
