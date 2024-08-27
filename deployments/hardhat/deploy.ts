f2c26af81fa8fd4b10977d0bc826c73ba5f42ad9
ErvinDimitri <ervinluis84@gmail.com>
2024-08-27 07:03:03 +0200

    Feature: Deployment

---
+++ deployments/hardhat/deploy.ts
@@ -0,0 +1,6 @@
+import { Deployed, DeploymentManager } from "../../plugins/deployment_manager";
+import { FaucetToken, SimplePriceFeed } from "../../build/types";
+
+export default async function deploy( deploymentManager: DeploymentManager, deploySpec: DeploySpec): Promise<Deployed>{
+
+}
